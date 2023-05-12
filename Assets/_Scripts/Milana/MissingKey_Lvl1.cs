d8a451b86e76bf4181412ca3fb61d54bf1d1b738
milanatran <milanatran.ntm@gmail.com>
2023-05-12 16:39:23 +0200

    Fixed display issue. Minigame works how it should

--- Assets/_Scripts/Milana/MissingKey_Lvl1.cs
+++ Assets/_Scripts/Milana/MissingKey_Lvl1.cs
@@ -16,35 +16,47 @@ public class MissingKey_Lvl1 : MonoBehaviour
     [SerializeField]
     private GameObject loseDisplay;
     [SerializeField]
-    private GameObject ButtonContainer;
-    bool playerPressed = false;
-    
+    private GameObject buttonContainer;
+    private bool _playerPressed = false;
+    private float _playfieldWidth;
+    private Bounds _cameraViewportBounds;
+    private Camera _mainCamera;
+
+
+    private void Awake()
+    {
+        _mainCamera = Camera.main;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        generatePattern();
-        displayPattern();
+        //playfieldWidth = transform.GetComponentInChildren<RectTransform>().rect.width;
+        _cameraViewportBounds = new Bounds(_mainCamera.transform.position, _mainCamera.ViewportToWorldPoint(new Vector3(1f, 1f, 0f)) - _mainCamera.ViewportToWorldPoint(Vector3.zero));
+        _playfieldWidth = _cameraViewportBounds.size.x;
+        //Debug.Log(_cameraViewportBounds.max.x);
+        GeneratePattern();
+        DisplayPattern();
     }
 
     // Update is called once per frame
     void Update()
     {
-        nextRound();
+        NextRound();
     }
 
     // Subscribes to playerPress()
     private void OnEnable()
     {
-        InputHandler.RightArrowBtnAction += playerPress;
-        InputHandler.LeftArrowBtnAction += playerPress;
-        InputHandler.UpArrowBtnAction += playerPress;
-        InputHandler.DownArrowBtnAction += playerPress;
+        InputHandler.RightArrowBtnAction += PlayerPress;
+        InputHandler.LeftArrowBtnAction += PlayerPress;
+        InputHandler.UpArrowBtnAction += PlayerPress;
+        InputHandler.DownArrowBtnAction += PlayerPress;
 
     }
 
     // Creates a new random pattern 
-    private void generatePattern()
+    private void GeneratePattern()
     {
         pattern.Clear();
         List<GameObject> usedButtons = new List<GameObject>();
@@ -70,30 +82,30 @@ public class MissingKey_Lvl1 : MonoBehaviour
         }
     }
 
-    private void displayPattern()
+    private void DisplayPattern()
     {
 
-        float canvasWidth = 1920;
+        //float canvasWidth = 1920;
         int count = pattern.Count;
-        float step = canvasWidth/(count+1); // Schrittlänge
-
+        float step = _playfieldWidth/(count+1); // Schrittlänge
         for(int i=0; i<count; i++)
         {
-            float buttonPosX = pattern[i].transform.localScale.x + step * (count + 1) - canvasWidth / 2;
-            Vector3 newPosition = ButtonContainer.transform.position + new Vector3(step * (count + 1) - canvasWidth / 2, 0, 0);
+            float buttonPosX = pattern[i].transform.localScale.x - 1 + step * (i + 1) - _playfieldWidth / 2;
+            
+            //Vector3 newPosition = ButtonContainer.transform.position + new Vector3(step * (count + 1) - playfieldWidth / 2, 0, 0);
             //float offset = pattern[i].transform.localScale.x  + startPoint * i;
-            Instantiate(pattern[i], newPosition, Quaternion.identity, ButtonContainer.transform);
+            Instantiate(pattern[i], new Vector3(buttonPosX, 0, 0), Quaternion.identity, buttonContainer.transform);
         }
     }
 
-    private bool checkWin()
+    private bool CheckWin()
     {
         
             //go through each button and check if one was deactivated
-            for (int i = 0; i < ButtonContainer.transform.childCount; i++)
+            for (int i = 0; i < buttonContainer.transform.childCount; i++)
             {
                
-                bool isActive = ButtonContainer.transform.GetChild(i).gameObject.activeInHierarchy;
+                bool isActive = buttonContainer.transform.GetChild(i).gameObject.activeInHierarchy;
 
                 //a button on the screen was deactivated -> lose 
                 if (!isActive)
@@ -107,40 +119,40 @@ public class MissingKey_Lvl1 : MonoBehaviour
         return true;
     }
 
-    private void deleteAll()
+    private void DeleteAll()
     {
-        for (int i = 0; i < ButtonContainer.transform.childCount; i++)
+        for (int i = 0; i < buttonContainer.transform.childCount; i++)
         {
-            Destroy(ButtonContainer.transform.GetChild(i).gameObject);
+            Destroy(buttonContainer.transform.GetChild(i).gameObject);
         }
     }
 
-    private void playerPress()
+    private void PlayerPress()
     {
-        playerPressed = true;
+        _playerPressed = true;
     }
 
-    private void nextRound()
+    private void NextRound()
     {
-        if (playerPressed)
+        if (_playerPressed)
         {
-            deleteAll();
-            if (checkWin())
+            DeleteAll();
+            if (CheckWin())
             {
-                generatePattern();
-                displayPattern();
+                GeneratePattern();
+                DisplayPattern();
                 
             }
             else
             {
                 loseDisplay.SetActive(true);
-                InputHandler.RightArrowBtnAction -= playerPress;
-                InputHandler.LeftArrowBtnAction -= playerPress;
-                InputHandler.UpArrowBtnAction -= playerPress;
-                InputHandler.DownArrowBtnAction -= playerPress;
+                InputHandler.RightArrowBtnAction -= PlayerPress;
+                InputHandler.LeftArrowBtnAction -= PlayerPress;
+                InputHandler.UpArrowBtnAction -= PlayerPress;
+                InputHandler.DownArrowBtnAction -= PlayerPress;
             }
         }
-        playerPressed = false;
+        _playerPressed = false;
     }
 
 }
