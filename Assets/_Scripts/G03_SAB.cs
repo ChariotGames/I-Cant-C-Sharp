2f54422d081e3f8b2f2d62ec8445097fd51a3e9e
Jannis Suchanek <jannis@suchanek-berlin.de>
2023-05-13 15:56:39 +0200

    UI elements & G03_SAB prefab
    added UI elements,
    added activating predefined gameobjects instead of sprites

--- Assets/_Scripts/G03_SAB.cs
+++ Assets/_Scripts/G03_SAB.cs
@@ -4,68 +4,49 @@ using System.Collections.Generic;
 using _Scripts._Input;
 using UnityEngine;
 using Random = UnityEngine.Random;
+using TMPro;
 
 public class G03_SAB : MonoBehaviour
 {
     [SerializeField] private int difficulty;
+    [SerializeField] private List<GameObject> options;
+    [SerializeField] private TMP_Text stepBackText;
     
     private bool isYes;
     private bool isNo;
     private int lastSprite;
     private int index;
-    private SpriteRenderer sprite;
-
-    [SerializeField] private List<Sprite> sprites = new List<Sprite>();
-
-    private void Awake()
-    {
-        GameObject symbol = new GameObject();
-        symbol.transform.Translate(0,0,0, Space.World);
-        symbol.SetActive(true);
-        sprite = symbol.AddComponent<SpriteRenderer>();
-    }
+    private int steps = 1;
+    
+    
     
     // Start is called before the first frame update
     void Start()
     {
-        spawnUIElements();
+        stepBackText.text += steps;
         StartCoroutine(GameStartCoroutine());
     }
     
-
-    // Update is called once per frame
-    void Update()
+    private void SpawnSymbol()
     {
-        
-    }
-    
-    private void spawnUIElements()
-    {
-        
-    }
-
-    private void spawnSymbol()
-    {
-        index = Random.Range(0, sprites.Count);
-        sprite.sprite = sprites[index];
-        sprite.gameObject.SetActive(true);
+        index = Random.Range(0, options.Count);
+        options[index].SetActive(true);
     }
 
     private IEnumerator GameStartCoroutine()
     {
-        spawnSymbol();
+        SpawnSymbol();
         lastSprite = index;
         yield return new WaitForSeconds(1);
-        sprite.gameObject.SetActive(false);
+        options[index].SetActive(false);
         StartCoroutine(SpawnCoroutine());
-        yield break;
     }
 
     private IEnumerator SpawnCoroutine()
     {
         while (true)
         {
-            if (sprite.gameObject.activeSelf)
+            if (options[0].activeSelf || options[1].activeSelf)
             {
                 Debug.Log(index + " " + lastSprite);
                 float timer = Time.unscaledTime;
@@ -73,7 +54,7 @@ public class G03_SAB : MonoBehaviour
                 if ((index == lastSprite && isYes && !isNo) || (index != lastSprite && isNo && !isYes))
                 {
                     Debug.Log("Win");
-                    sprite.gameObject.SetActive(false);
+                    options[index].SetActive(false);
                     isYes = false;
                     isNo = false;
                     lastSprite = index;
@@ -81,14 +62,14 @@ public class G03_SAB : MonoBehaviour
                 else
                 {
                     Debug.Log("Lose Condition");
-                    sprite.gameObject.SetActive(false);
+                    options[index].SetActive(false);
                     yield break;
                 }
             }
             else
             {
                 yield return new WaitForSeconds(1);
-                spawnSymbol();
+                SpawnSymbol();
             }
         }
     }
