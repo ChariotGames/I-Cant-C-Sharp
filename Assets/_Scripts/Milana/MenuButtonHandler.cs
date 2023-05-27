f47fe3dc03c1404ccd69fcabbb2dd816a4434b0f
milanatran <milanatran.ntm@gmail.com>
2023-05-27 13:29:46 +0200

    Deleted old scenes and added relevant buttons

--- Assets/_Scripts/Milana/MenuButtonHandler.cs
+++ Assets/_Scripts/Milana/MenuButtonHandler.cs
@@ -12,13 +12,22 @@ public class MenuButtonHandler : MonoBehaviour
     public GameObject quitButton;
     public GameObject singlePlayerButton;
     public GameObject multiplayerButton;
-    private string playerMode;
-    private bool isClicked;
+    public GameObject selectGameButton;
+    public GameObject endlessModeButton;
+    public GameObject backButton;
+    private List<string> playerMode;
 
     private void Awake()
     {
-        singlePlayerButton.SetActive(false);
+        startButton.SetActive(true);
+        optionsButton.SetActive(true);
+        quitButton.SetActive(true);
+        /*singlePlayerButton.SetActive(false);
         multiplayerButton.SetActive(false);
+        selectGameButton.SetActive(false);
+        endlessModeButton.SetActive(false);
+        backButton.SetActive(false);
+        */
     }
 
     // Start is called before the first frame update
@@ -29,28 +38,20 @@ public class MenuButtonHandler : MonoBehaviour
         startButton.GetComponentInChildren<Button>().onClick.AddListener(OnStartButtonClick);
         singlePlayerButton.GetComponentInChildren<Button>().onClick.AddListener(OnSinglePlayerButtonClick);
         multiplayerButton.GetComponentInChildren<Button>().onClick.AddListener(OnMultiplayerButtonClick);
+        selectGameButton.GetComponentInChildren<Button>().onClick.AddListener(OnSelectGameButtonClick);
+        endlessModeButton.GetComponentInChildren<Button>().onClick.AddListener(OnEndlessModeButtonClick);
+        backButton.GetComponentInChildren<Button>().onClick.AddListener(OnBackButtonClick);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //TODO: Doesn't work .___.
-       /* if (isClicked)
-        {
-            startButton.SetActive(false);
-            optionsButton.SetActive(false);
-            quitButton.SetActive(false);
-            singlePlayerButton.SetActive(true);
-            multiplayerButton.SetActive(true);
-            isClicked = false;
-        }*/
+
     }
 
    
     public void OnStartButtonClick() 
     {
-        Debug.Log("Clicked Start!");
-        //isClicked = true;
         startButton.SetActive(false);
         optionsButton.SetActive(false);
         quitButton.SetActive(false);
@@ -58,13 +59,34 @@ public class MenuButtonHandler : MonoBehaviour
         multiplayerButton.SetActive(true);
     }
 
-    private void OnSinglePlayerButtonClick()
+    public void OnSinglePlayerButtonClick()
     {
-        playerMode = "Single Player";
+        
+        Debug.Log("Single Player clicked!");
+        singlePlayerButton.SetActive(false);
+        multiplayerButton.SetActive(false);
+        selectGameButton.SetActive(true);
+        endlessModeButton.SetActive(true);
+        backButton.SetActive(true);
+
     }
 
-    private void OnMultiplayerButtonClick()
+    public void OnMultiplayerButtonClick()
     {
-        playerMode = "Multiplayer";
+        //playerMode.Add("Multiplayer");
+    }
+
+    public void OnSelectGameButtonClick()
+    {
+
+    }
+
+    public void OnEndlessModeButtonClick()
+    {
+
+    }
+    public void OnBackButtonClick()
+    {
+
     }
 }
