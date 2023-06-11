8943056430c1ce9b960742d04ee61c3675ab9934
Jannis Suchanek <119596476+boTimPact@users.noreply.github.com>
2023-06-12 00:20:48 +0200

    Restore RapidReflex Fields

--- Assets/_Scripts/Games/RapidReflexRestore.cs
+++ Assets/_Scripts/Games/RapidReflexRestore.cs
@@ -2,6 +2,24 @@ namespace _Scripts.Games
 {
     public class RapidReflexRestore
     {
+        [SerializeField] private Difficulty difficulty;
+        [SerializeField] private int timeToAnswerInMs;
+        [SerializeField] private float lightTimer;
+        [SerializeField] private GameObject lightsTop, lightsBottom, light, overlayContainer, background;
+        [SerializeField] private Color darkRed, lightRed, darkGreen, lightGreen;
+        [SerializeField] private List<Color> flashColor;
+        [SerializeField] private TMP_Text gameStateText;
+        
+        
+        private const int NUMBER_LIGHTS_ROW = 5;
+        private SpriteRenderer[] bulbsSpriteTop = new SpriteRenderer[NUMBER_LIGHTS_ROW];
+        private SpriteRenderer[] bulbsSpriteBottom = new SpriteRenderer[NUMBER_LIGHTS_ROW];
+        private SpriteRenderer backroundSprite;
+        private float timeElapsed = 0;
+        private bool isButtonPressed = false;
+        private float randomDelay = 0;
+        
+        
         if (difficulty == Difficulty.LVL3 && i == NUMBER_LIGHTS_ROW-1) StartCoroutine(randomDistraction());
         
         
