80c0c8be6aa879f3e463e86e193a3c0d42e6f7f2
Jannis Suchanek <119596476+boTimPact@users.noreply.github.com>
2023-06-29 12:38:19 +0200

    rename variable

--- Assets/_Scripts/Games/RapidReflex.cs
+++ Assets/_Scripts/Games/RapidReflex.cs
@@ -37,7 +37,7 @@ namespace _Scripts.Games
         private const int NUMBER_LIGHTS = 5;
         private float _timeElapsed = 0, _randomDelay = 0;
         private bool _isButtonPressed = false;
-        private SpriteRenderer _backroundSprite;
+        private SpriteRenderer _backgroundSprite;
         
 
     #endregion Fields
@@ -48,7 +48,7 @@ namespace _Scripts.Games
         {
             flashColor.Add(lightRed);
             flashColor.Add(lightGreen);
-            _backroundSprite = background.GetComponent<SpriteRenderer>();
+            _backgroundSprite = background.GetComponent<SpriteRenderer>();
             _bulbsSpriteTop = SpawnLights(NUMBER_LIGHTS, darkRed, lightsTop.transform);
             _bulbsSpriteBottom = SpawnLights(NUMBER_LIGHTS, darkGreen, lightsBottom.transform);
             StartCoroutine(GameCoroutine());
@@ -173,7 +173,7 @@ namespace _Scripts.Games
 
         private IEnumerator FlashBackground()
         {
-            _backroundSprite.color = flashColor[Random.Range(0, flashColor.Count)];
+            _backgroundSprite.color = flashColor[Random.Range(0, flashColor.Count)];
             background.SetActive(true);
             yield return new WaitForSeconds(0.2f);
             background.SetActive(false);
