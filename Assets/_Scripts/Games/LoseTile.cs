040f78c2f0d0ee1a8318e33382df3bc6cd176398
Bigfoot52 <delafosse.jerome@web.de>
2023-06-29 16:54:01 +0200

    Basic game functions LoseTile

---
+++ Assets/_Scripts/Games/LoseTile.cs
@@ -0,0 +1,106 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using _Scripts._Input;
+using _Scripts.Models;
+
+namespace _Scripts.Games
+{
+    public class LoseTile : BaseGame
+    {
+        #region Serialized Fields
+
+        [SerializeField][Range(3,5)] private int gridSize = 3;
+        [SerializeField][Range(1,2)] private float space = 1;
+        [SerializeField] private GameObject tile, player;
+        [SerializeField] private Transform container;
+
+        #endregion Serialized Fields
+
+        #region Fields
+
+        [SerializeField]private int remainingTiles = 0, ammountOfTiles = 0, lives = 3;
+        #endregion Fields
+
+        #region Built-Ins / MonoBehaviours
+
+        void Start()
+        {
+            ammountOfTiles = gridSize * gridSize;
+            remainingTiles = ammountOfTiles;
+            SetGrid();
+        }
+
+        void Update()
+        {
+            
+        }
+
+
+        #endregion Built-Ins / MonoBehaviours
+
+        #region GetSets / Properties
+
+
+
+        #endregion GetSets / Properties
+
+        #region Game Mechanics / Methods
+
+
+
+        public void PlayerTouched(GameObject obj)
+        {
+            ElementType type = obj.GetComponent<LoseTileTile>().type;
+
+            switch (type)
+            {
+                case ElementType.CHECKPOINT:
+                    //Destroy(obj);
+                    remainingTiles--;
+                    if (remainingTiles == ammountOfTiles - 1)
+                    {
+                        base.Win();
+                    }
+                    break;
+                case ElementType.ENEMY:
+                    lives--;
+                    if (lives == 0)
+                    {
+                        base.Lose();
+                    }
+                    break;
+                case ElementType.GOAL:
+                    //if (remainingTiles == ammountOfTiles -1)
+                    //{
+                    //    base.Win();
+                    //}
+                    break;
+            }
+        }
+
+        #endregion Game Mechanics / Methods
+
+        #region Overarching Methods / Helpers
+
+        private void SetGrid()
+        {
+            for (int x = -gridSize/2; x <= gridSize/2; x++)
+
+            {
+                for (int y = -gridSize/2; y <= gridSize/2; y++)
+                {
+                    GameObject obj = Instantiate(tile, container);
+                    obj.transform.Translate(x * space, y * space, 0);
+                }
+                //GameObject obj = Instantiate(tile, container);
+                //obj.transform.SetParent(container, true); 
+            }
+        }
+
+        
+
+        #endregion Overarching Methods / Helpers
+    }
+}
---
+++ Assets/_Scripts/Jerome/LoseTilePlayer.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using _Scripts._Input;
+
+namespace _Scripts.Games
+{
+    public class LoseTilePlayer : MonoBehaviour
+    {
+        #region Serialized Fields
+        [SerializeField] LoseTile MainGame;
+        [SerializeField] float speed = 5f;
+
+
+        #endregion Serialized Fields
+
+        #region Fields
+
+
+
+        #endregion Fields
+
+        #region Built-Ins / MonoBehaviours
+
+        void Start()
+        {
+            
+        }
+
+        void Update()
+        {
+            gameObject.transform.Translate(InputHandler.StickLeft * Time.deltaTime * speed);
+           
+        }
+
+        #endregion Built-Ins / MonoBehaviours
+
+        #region GetSets / Properties
+        
+        
+
+        #endregion GetSets / Properties
+
+        #region Game Mechanics / Methods
+        
+        
+
+        public void TemplateMethod(bool param)
+        {
+            
+        }
+
+        #endregion Game Mechanics / Methods
+
+        #region Overarching Methods / Helpers
+        
+        
+
+        #endregion Overarching Methods / Helpers
+    }
+}
