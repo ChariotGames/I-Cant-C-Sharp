190922b5a868047a58bbd4302a454d9dc98d750f
Jannis Suchanek <jannis@suchanek-berlin.de>
2023-07-03 16:15:48 +0200

    added namespace

--- Assets/Scripts/Controllers/EndScreen.cs
+++ Assets/Scripts/Controllers/EndScreen.cs
@@ -7,9 +7,11 @@ using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
-public class EndScreen : MonoBehaviour
+namespace Scripts.Controllers
 {
-    #region Serialized Fields
+    public class EndScreen : MonoBehaviour
+    {
+        #region Serialized Fields
 
         [SerializeField] private InputActionAsset playerInput;
         [SerializeField] private GameObject restartButton;
@@ -21,15 +23,16 @@ public class EndScreen : MonoBehaviour
         [SerializeField] private TMP_Text timerText;
 
 
-    #endregion Serialized Fields
+        #endregion Serialized Fields
 
-    #region Fields
+        #region Fields
 
         private InputActionMap playerMap, uiMap;
 
-    #endregion Fields
-    
-    #region Built-Ins / MonoBehaviours
+        #endregion Fields
+
+        #region Built-Ins / MonoBehaviours
+
         private void Awake()
         {
             scoreText.text = "Score: " + score.text;
@@ -38,24 +41,24 @@ public class EndScreen : MonoBehaviour
             playerMap = playerInput.actionMaps[0];
             uiMap = playerInput.actionMaps[1];
             uiMap.Enable();
-            
+
         }
 
         void Start()
         {
-            
+
         }
 
-    #endregion Built-Ins / MonoBehaviours
+        #endregion Built-Ins / MonoBehaviours
 
-    #region Game Mechanics / Methods
-    
+        #region Game Mechanics / Methods
+
         public void Restart()
         {
             settings.Lives = defaultSettings.Lives;
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
-        
+
         public void GoToMenu()
         {
             SceneChanger.ChangeScene(0);
@@ -68,20 +71,22 @@ public class EndScreen : MonoBehaviour
             #endif
                 Application.Quit();
         }
-            
-    #endregion Game Mechanics / Methods
-        
-    #region Overarching Methods / Helpers
+
+        #endregion Game Mechanics / Methods
+
+        #region Overarching Methods / Helpers
+
         private void OnEnable()
         {
             playerMap.Disable();
             EventSystem.current.SetSelectedGameObject(restartButton);
         }
-        
+
         private void OnDisable()
         {
-            
+
         }
-        
-    #endregion Overarching Methods / Helpers
+
+        #endregion Overarching Methods / Helpers
+    }
 }
