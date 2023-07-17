e616a63db1d83842c6031a1ada006d659341d268
Larry <104570219+Larry-Master@users.noreply.github.com>
2023-07-17 13:01:21 +0200

    Added direct Back on Click

--- Assets/BackButton.cs
+++ Assets/BackButton.cs
@@ -1,17 +1,22 @@
 using Scripts._Input;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class BackButton : MonoBehaviour
 {
+    private Button backButton;
     private void OnEnable()
     {
         InputHandler.ButtonEast += SelectBackButton;
+        backButton = GetComponent<Button>();
+
     }
 
     private void SelectBackButton()
     {
         EventSystem.current.SetSelectedGameObject(this.gameObject);
+        backButton.onClick.Invoke();
     }
 
     private void OnDisable()
