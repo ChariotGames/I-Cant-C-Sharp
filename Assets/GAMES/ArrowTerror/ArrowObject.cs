dc3581597bb6e93c837857a5fb402b630b0f5fb3
Alexander Stämmler <83005986+AlexanderStae@users.noreply.github.com>
2023-07-22 13:01:17 +0200

    ArrowTerror fish look at the boat now.

--- Assets/GAMES/ArrowTerror/ArrowObject.cs
+++ Assets/GAMES/ArrowTerror/ArrowObject.cs
@@ -27,7 +27,20 @@ namespace Scripts.Games
         public void PlayerMoved(Vector3 playerPosition)
         {
             transform.position = Vector3.MoveTowards(transform.position, playerPosition, EnemySpeed * Time.deltaTime);
-
+            Vector3 directionToTarget = playerPosition - transform.position;
+            float angle = Mathf.Atan2(directionToTarget.y, directionToTarget.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            gameObject.GetComponent<SpriteRenderer>().flipY = false;
+            transform.GetChild(0).GetComponent<SpriteRenderer>().flipY = false;
+            //gameObject.GetComponent<SpriteRenderer>().flipX = false;
+            //transform.GetChild(0).GetComponent<SpriteRenderer>().flipX = false;
+            if (Mathf.Abs(angle) <= 180 && Mathf.Abs(angle) >= 90)
+            {
+                gameObject.GetComponent<SpriteRenderer>().flipY = true;
+                transform.GetChild(0).GetComponent<SpriteRenderer>().flipY = true;
+                //gameObject.GetComponent<SpriteRenderer>().flipX = true;
+                //transform.GetChild(0).GetComponent<SpriteRenderer>().flipX = true;
+            }
         }
     }
 }
