cf8e50b1ef0a911b64b580ca8b3322f6dd8c221f
Bigfoot52 <delafosse.jerome@web.de>
2023-07-26 19:31:09 +0200

    Lose Tiles Movement update
    not ready jet

--- Assets/GAMES/LoseTile/LoseTile.cs
+++ Assets/GAMES/LoseTile/LoseTile.cs
@@ -25,8 +25,8 @@ namespace Scripts.Games
 
         private void Awake()
         {
-            successesToWin = 3;
-            failsToLose = 1;
+            //successesToWin = 3;
+            //failsToLose = 1;
             ammountOfTiles = gridSize * gridSize;
             player.Stick = _keys.One.Input;
         }
