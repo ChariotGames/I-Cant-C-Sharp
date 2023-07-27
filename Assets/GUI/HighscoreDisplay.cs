81100242c0eb13028ce83f1d6212e2d1731fc7f1
n-c0de-r <78597469+n-c0de-r@users.noreply.github.com>
2023-07-27 20:37:54 +0200

    more highscore stuff

--- Assets/GUI/HighscoreDisplay.cs
+++ Assets/GUI/HighscoreDisplay.cs
@@ -6,17 +6,43 @@ namespace Scripts.Controllers
 {
     public class HighscoreDisplay : MonoBehaviour
     {
-        [SerializeField] private Transform namesContainer, scoresContainer;
-        [SerializeField] private GameObject templateName, templateScore;
+        [SerializeField] private Transform namesContainer, scoresContainer, timesContainer;
+        [SerializeField] private GameObject templateName, templateScore, templateTime;
         [SerializeField] private Settings settings;
+
+        private const int COUNT = 5;
+
+        private struct Score
+        {
+            public string name;
+            public int value;
+        }
 
         private void OnEnable()
         {
             DisplayHighscores();
         }
 
-        public void DisplayHighscores()
+        private void OnDisable()
         {
+            ClearContainers();
+        }
+
+        private void ClearContainers()
+        {
+            for(int i = 0; i < COUNT; i++)
+            {
+                Destroy(namesContainer.GetChild(i).gameObject);
+                Destroy(scoresContainer.GetChild(i).gameObject);
+            }
+        }
+
+        private void DisplayHighscores()
+        {
+            for (int i = 1; i <= COUNT; i++)
+            {
+
+            }
             GameObject name = Instantiate(templateName, namesContainer);
             name.GetComponent<TMP_Text>().text = settings.SelectedCharacter.Name;
             GameObject score = Instantiate(templateScore, scoresContainer);
