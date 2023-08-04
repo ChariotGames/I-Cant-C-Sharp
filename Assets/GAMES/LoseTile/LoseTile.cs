597a2b0e07d1ae3be1e24b9dcc778b8d1b678d5d
Bigfoot52 <delafosse.jerome@web.de>
2023-08-04 07:43:25 +0200

    Update LoseTile.cs

--- Assets/GAMES/LoseTile/LoseTile.cs
+++ Assets/GAMES/LoseTile/LoseTile.cs
@@ -1,4 +1,3 @@
-//using System;
 using UnityEngine;
 using Scripts.Models;
 using System.Collections;
@@ -16,6 +15,7 @@ namespace Scripts.Games
         [SerializeField] private GameObject tile, dangerzone;
         [SerializeField] private LoseTilePlayer player;
         [SerializeField] private Transform container;
+
         //[SerializeField] AudioSource WinSound;
 
 
@@ -25,13 +25,16 @@ namespace Scripts.Games
 
         [SerializeField] private int remainingTiles = 0, ammountOfTiles = 0, lives = 3;
         private int gridHalf;
-        private int removeCount;
+        private bool[,] grid;
+
         #endregion Fields
 
         #region Built-Ins / MonoBehaviours
 
         private void Awake()
         {
+            grid = new bool[gridSize, gridSize];
+
             failsToLose = 3;
             if (Difficulty.EASY == difficulty)
             {
@@ -45,7 +48,6 @@ namespace Scripts.Games
         IEnumerator Start()
         {
             yield return StartCoroutine(AnimateInstruction());
-
             ResetGame();
         }
 
@@ -66,7 +68,7 @@ namespace Scripts.Games
                     {
                         base.Success();
                         //WinSound.Play();
-                        Invoke(nameof(ResetGame),1);
+                        Invoke(nameof(ResetGame), 1);
                     }
                     break;
                 case ElementType.ENEMY:
@@ -90,50 +92,82 @@ namespace Scripts.Games
 
         private void SetGrid()
         {
-            removeCount = ((int)difficulty);
-            if (difficulty == Difficulty.HARD)
-            {
-                removeCount = Random.Range(4, ((int)difficulty)*2) - 1;
-            }
-            if (difficulty == Difficulty.EASY) 
-            {
-                removeCount = 0;
-            }
-            remainingTiles -= removeCount;
             for (int x = -gridHalf; x <= gridHalf; x++)
             {
                 for (int y = -gridHalf; y <= gridHalf; y++)
                 {
-
                     GameObject obj = Instantiate(tile, container);
                     obj.transform.Translate(x * space, y * space, 0);
                     obj.SetActive(true);
-                    int Chance = Random.Range(0, 2);
-                    if (Chance == 1 && removeCount > 0)
-                    {
-                        obj.GetComponent<LoseTileField>().setInVisible();
-                        obj.GetComponent<LoseTileField>().visited = true;
-                        obj.GetComponent<LoseTileField>().type = ElementType.ENEMY;
-                        removeCount--;
-                    }
-
+                    grid[x + gridHalf, y + gridHalf] = true;
                 }
             }
+            
+            if (difficulty == Difficulty.MEDIUM) RemoveTiles(Random.Range(2, 4));
+            if (difficulty == Difficulty.HARD) RemoveTiles(Random.Range(4, 6));
             SetPlayer();
+        }
+
+        private void RemoveTiles(int removeCount)
+        {
+            remainingTiles -= removeCount;
+            while (removeCount > 0)
+            {
+                int x = Random.Range(0, gridSize);
+                int y = Random.Range(0, gridSize);
+
+                if (CheckDiagonals(x, y) && CheckVertical(x, y) && CheckHorizontal(x, y))
+                {
+                    int pos = y * gridSize + x;
+                    GameObject obj = container.GetChild(pos).gameObject;
+                    LoseTileField LTF = obj.GetComponent<LoseTileField>();
+                    LTF.setInVisible();
+                    LTF.visited = true;
+                    LTF.type = ElementType.ENEMY;
+                    grid[x, y] = false; 
+                    removeCount--;
+                }
+            }
+        }
+
+        private bool CheckHorizontal(int x, int y)
+        {
+            bool result = false;
+            if ((x - 1) >= 0) result |= grid[(x - 1), y];
+            if ((x + 1) < gridSize) result |= grid[(x + 1), y];
+            return result;
+        }
+
+        private bool CheckVertical(int x, int y)
+        {
+            bool result = false;
+            if ((y - 1) >= 0) result |= grid[x, (y - 1)];
+            if ((y + 1) < gridSize) result |= grid[x, (y + 1)];
+            return result;
+        }
+
+        
+        private bool CheckDiagonals(int x, int y)
+        {
+            bool result = true;
+            if ((x - 1) >= 0 && (y - 1) >= 0) result &= grid[(x - 1), (y - 1)];
+            if ((x + 1) < gridSize && (y - 1) >= 0) result &= grid[(x + 1), (y - 1)];
+            if ((x - 1) >= 0 && (y + 1) < gridSize) result &= grid[(x - 1), (y + 1)];
+            if ((x + 1) < gridSize && (y + 1) < gridSize) result &= grid[(x + 1), (y + 1)];
+            return result;
         }
 
         private void SetPlayer()
         {
-            int PlayerX = Random.Range(-gridHalf, +gridHalf + 1);
-            int PlayerY = Random.Range(-gridHalf, +gridHalf + 1);
-            LoseTileField Tile = container.GetChild((PlayerY + gridHalf) * gridSize + (PlayerX + gridHalf)).GetComponent<LoseTileField>();
-            while (!Tile.isVisable)
+            int startTile = Random.Range(0, container.childCount);
+            LoseTileField LTF = container.GetChild(startTile).gameObject.GetComponent<LoseTileField>();
+            while (!LTF.isVisable)
             {
-                PlayerX = Random.Range(-gridHalf, +gridHalf + 1);
-                PlayerY = Random.Range(-gridHalf, +gridHalf + 1);
-                Tile = container.GetChild((PlayerY +gridHalf) * gridSize + (PlayerX + gridHalf)).GetComponent<LoseTileField>();
+                startTile = Random.Range(0, container.childCount);
+                LTF = container.GetChild(startTile).gameObject.GetComponent<LoseTileField>();
             }
-            player.transform.localPosition = new Vector3(PlayerX * space, PlayerY * space, 0);
+
+            player.transform.localPosition = LTF.gameObject.transform.localPosition;
         }
 
         private void ResetGame()
